423d49550ff486778e2d037755928552d0a93445
Maria_Lazaro <mariaarino93@gmail.com>
2018-12-30 11:20:50 +0100

    extends query interface

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SearchController {
@@ -15,11 +16,13 @@ public class SearchController {
 
   @Autowired
   public SearchController(ProducerTemplate producerTemplate) {
+
     this.producerTemplate = producerTemplate;
   }
 
   @RequestMapping("/")
   public String index() {
+
     return "index";
   }
 
@@ -27,6 +30,19 @@ public class SearchController {
   @RequestMapping(value = "/search")
   @ResponseBody
   public Object search(@RequestParam("q") String q) {
-    return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+
+    int N = 0;
+    if (q.contains("max:")) {
+      int pos_max = q.indexOf("max");
+      String values = q.substring(pos_max);
+      String[] div = values.split(":");
+      N = Integer.parseInt(div[1]);
+    }
+    // Now we create the headers
+    Map<String,Object> headers = new HashMap<>();
+    q = q.replaceAll("max:[0-9]*","");
+    headers.put("CamelTwitterKeywords",q);
+    headers.put("CamelTwitterCount",N);
+    return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
   }
 }
